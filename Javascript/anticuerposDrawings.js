9e1c429de6500435244583e6ebcc90c0265b3ad5
mortaldraw <robildruirey@hotmail.com>
2015-02-20 18:06:28 -0600

    2nda entrega
    - Agregar AnticuerposGP.html
    - Cambiar nombre de página principal
    - Agregar archivo de la plantilla (Para trabajo offline)
    - Cambiar algunos detalles del formato de página

---
+++ Javascript/anticuerposDrawings.js
@@ -0,0 +1,518 @@
+var arrAnticuerpos = [{
+		"mAb": "1H3",
+		"inicio": 267,
+		"fin": 281,
+		"GPRegion": "Z:G1",
+		"Conservation": "100.00%",
+		"EpType": "Linear",
+		"Level": 1
+	},
+	{
+		"mAb": "2G4",
+		"inicio": 501,
+		"fin": 515,
+		"GPRegion": "Z:G2",
+		"Conservation": "93.30%",
+		"EpType": "Linear",
+		"Level": 4
+	},
+	{
+		"mAb": "4G7",
+		"inicio": 501,
+		"fin": 515,
+		"GPRegion": "Z:G2",
+		"Conservation": "93.30%",
+		"EpType": "Linear",
+		"Level": 5
+	},
+	{
+		"mAb": "5D2",
+		"inicio": 329,
+		"fin": 343,
+		"GPRegion": "Z:G1",
+		"Conservation": "73.30%",
+		"EpType": "Linear",
+		"Level": 7
+	},
+	{
+		"mAb": "5.00E+06",
+		"inicio": 401,
+		"fin": 415,
+		"GPRegion": "Z:G1",
+		"Conservation": "80.00%",
+		"EpType": "Linear",
+		"Level": 1
+	},
+	{
+		"mAb": "7C9",
+		"inicio": 157,
+		"fin": 369,
+		"GPRegion": "Z,GP1",
+		"Conservation": "91.50%",
+		"EpType": "Conformational",
+		"Level": 4
+	},
+	{
+		"mAb": "7G4",
+		"inicio": 157,
+		"fin": 369,
+		"GPRegion": "Z,GP1",
+		"Conservation": "91.50%",
+		"EpType": "Conformational",
+		"Level": 5
+	},
+	{
+		"mAb": "10C3",
+		"inicio": 157,
+		"fin": 369,
+		"GPRegion": "Z,GP1",
+		"Conservation": "91.50%",
+		"EpType": "Conformational",
+		"Level": 6
+	},
+	{
+		"mAb": "KZ52",
+		"inicio": 42,
+		"fin": 43,
+		"GPRegion": "Z:GP1 GP2",
+		"Conservation": "100.00%",
+		"EpType": "Conformational",
+		"Level": 1
+	},
+	{
+		"mAb": "KZ52",
+		"inicio": 505,
+		"fin": 513,
+		"GPRegion": "Z:GP1 GP2",
+		"Conservation": "100.00%",
+		"EpType": "Conformational",
+		"Level": 1
+	},
+	{
+		"mAb": "KZ52",
+		"inicio": 549,
+		"fin": 556,
+		"GPRegion": "Z:GP1 GP2",
+		"Conservation": "100.00%",
+		"EpType": "Conformational",
+		"Level": 1
+	},
+	{
+		"mAb": "13F6",
+		"inicio": 401,
+		"fin": 417,
+		"GPRegion": "Z,GP1",
+		"Conservation": "76.40%",
+		"EpType": "Linear",
+		"Level": 5
+	},
+	{
+		"mAb": "6D8",
+		"inicio": 389,
+		"fin": 405,
+		"GPRegion": "Z,GP1",
+		"Conservation": "82.40%",
+		"EpType": "Linear",
+		"Level": 4
+	},
+	{
+		"mAb": "12B5",
+		"inicio": 477,
+		"fin": 493,
+		"GPRegion": "Z,GP1",
+		"Conservation": "94.10%",
+		"EpType": "Linear",
+		"Level": 7
+	},
+	{
+		"mAb": "6.00E+03",
+		"inicio": 401,
+		"fin": 417,
+		"GPRegion": "Z:GP1",
+		"Conservation": "76.40%",
+		"EpType": "Linear",
+		"Level": 7
+	},
+	{
+		"mAb": "13C6",
+		"inicio": 392,
+		"fin": 479,
+		"GPRegion": "Z,S,IC:GP1,GP2, sGP",
+		"Conservation": "93.60%",
+		"EpType": "Conformational",
+		"Level": 6	
+	},
+	{
+		"mAb": "13C6",
+		"inicio": 494,
+		"fin": 635,
+		"GPRegion": "Z,S,IC:GP1,GP2, sGP",
+		"Conservation": "93.60%",
+		"EpType": "Conformational",
+		"Level": 6	
+	},
+	{
+		"mAb": "6D3",
+		"inicio": 302,
+		"fin": 479,
+		"GPRegion": "Z,S,IC:GP1,GP2 sGP",
+		"Conservation": "93.60%",
+		"EpType": "Conformational",
+		"Level": 3
+	},
+	{
+		"mAb": "6D3",
+		"inicio": 494,
+		"fin": 635,
+		"GPRegion": "Z,S,IC:GP1,GP2 sGP",
+		"Conservation": "93.60%",
+		"EpType": "Conformational",
+		"Level": 3
+	},
+	{
+		"mAb": "133/3.16",
+		"inicio": 521,
+		"fin": 560,
+		"GPRegion": "Z:GP2",
+		"Conservation": "97.50%",
+		"EpType": "Conformational",
+		"Level": 7
+	},
+	{
+		"mAb": "226/8.1",
+		"inicio": 1,
+		"fin": 232,
+		"GPRegion": "Z:GP1,sGP",
+		"Conservation": "97.40%",
+		"EpType": "Conformational",
+		"Level": 2
+	},
+	{
+		"mAb": "JP3K11",
+		"inicio": 302,
+		"fin": 479,
+		"GPRegion": "Z:GP1 GP2, sGP",
+		"Conservation": "90.00%",
+		"EpType": "Conformational",
+		"Level": 2
+	},
+	{
+		"mAb": "JP3K11",
+		"inicio": 505,
+		"fin": 514,
+		"GPRegion": "Z:GP1 GP2, sGP",
+		"Conservation": "90.00%",
+		"EpType": "Conformational",
+		"Level": 2
+	},
+	{
+		"mAb": "JP3K11",
+		"inicio": 549,
+		"fin": 556,
+		"GPRegion": "Z:GP1 GP2, sGP",
+		"Conservation": "90.00%",
+		"EpType": "Conformational",
+		"Level": 2
+	},
+	{
+		"mAb": "S9",
+		"inicio": 293,
+		"fin": 307,
+		"GPRegion": "Z:GP1,sGP",
+		"Conservation": "100.00%",
+		"EpType": "Linear",
+		"Level": 1
+	}];
+
+const ANTICUERPOSCOUNT = arrAnticuerpos.length;
+
+$(document).ready(function () {
+	var tabla = $("table.tablaProteinas");
+	tabla.html("<tr><th>mAb</th><th>Start</th><th>End</th><th>Specific strain: GP region</th><th>Percentage of conservation</th><th>Epitope type</th></tr>");
+	for (var ii = 0; ii < ANTICUERPOSCOUNT; ii++) {
+		var builtHtml = "<tr><td>" + arrAnticuerpos[ii].mAb + "</td><td>" + arrAnticuerpos[ii].inicio + "</td><td>" + arrAnticuerpos[ii].fin + "</td><td>" + arrAnticuerpos[ii].GPRegion + "</td><td>" + arrAnticuerpos[ii].Conservation + "</td><td>" + arrAnticuerpos[ii].EpType + "</td></tr>";
+		tabla.html(tabla.html() + builtHtml);
+	}
+});
+
+
+
+var ANCHO = 45;
+var ANCHOTOTAL = 55;
+var ESPACIADO = 5;
+var RADIO = 3;
+var TEXT = "12px Arial";
+var TEXTCOLOR = "#000000";
+
+
+function init() {
+	// Crear el stage para dibujar sobre él
+	var stage = new createjs.Stage("drawingCanvas");
+
+	// Crear variables para las funciones que dibujan rectángulos de selección
+	var rec = new createjs.Shape();
+	var rec2 = new createjs.Shape();
+	var posIni;
+	var posFin;
+
+	// Crear arreglos de containers
+	var containers = new Array();
+	var shapes = new Array();
+	var tags = new Array();
+
+	// Crear GP superior
+	var MAINGP = new createjs.Container();
+	var GPSHAPE = new createjs.Shape();
+	GPSHAPE.graphics.beginFill("#cab2d6").drawRoundRect(0, ESPACIADO, 680, ANCHO, RADIO);
+	var GPLABEL = new createjs.Text("GP", TEXT, TEXTCOLOR);
+	GPLABEL.textAlign = "center";
+	GPLABEL.y = ANCHOTOTAL / 2 - 7;
+	GPLABEL.x = 340;
+
+	MAINGP.addChild(GPSHAPE, GPLABEL);
+	stage.addChild(MAINGP);
+	// Termina creación de GP superior 
+
+	for (var z = 0; z < ANTICUERPOSCOUNT ; z++) {
+		var j = arrAnticuerpos[z];
+		tags.push(new createjs.Text(j.mAb, TEXT, TEXTCOLOR));
+		shapes.push(new createjs.Shape());
+		containers.push(new createjs.Container());
+
+		shapes[z].graphics.beginFill(getColor(z)).drawRoundRect(j.inicio, ANCHOTOTAL * j.Level + ESPACIADO, j.fin - j.inicio, ANCHO, RADIO);
+
+		tags[z].textAlign = "center";
+
+		tags[z].y = ANCHOTOTAL * j.Level + ANCHOTOTAL / 2 - 6;
+		tags[z].x = (j.fin + j.inicio) / 2;
+		containers[z].addChild(shapes[z], tags[z]);
+
+	}
+
+	for (var j = 0; j < ANTICUERPOSCOUNT; j++) {
+		var z = containers[j];
+		stage.addChild(z);
+	}
+	changeDescription(0);
+
+	// Agregar listener de eventos
+	stage.on("stagemousedown", mouseDown);
+	stage.on("stagemouseup", mouseUp);
+
+	stage.addChild(rec);
+	stage.addChild(rec2);
+
+
+	function mouseDown(event) {
+		rec.graphics.clear();
+		rec2.graphics.clear();
+		posIni = event.stageX;
+		rec.graphics.beginFill("rgba(0, 0, 0, .25").drawRect(0, 0, posIni, 500);
+
+		stage.addChild(rec);
+		stage.update();
+	};
+
+	// Esto sucede al momento de que el click es liberado
+	function mouseUp(event) {
+		rec2.graphics.clear();
+		posFin = event.stageX;
+		if (posFin > posIni) {
+			rec2.graphics.beginFill("rgba(0, 0, 0, .25").drawRect(posFin, 0, 700 - posFin, 500);
+			var x = filter(posIni, posFin);
+			repopulateTable(x);
+		} else {
+			rec.graphics.clear();
+		}
+		stage.addChild(rec2);
+		stage.update();
+	};
+	
+	containers[0].on("click", function (evt) {
+		changeDescription(0);
+		stage.update();
+	});
+	containers[1].on("click", function (evt) {
+		changeDescription(1);
+		stage.update();
+	});
+	containers[2].on("click", function (evt) {
+		changeDescription(2);
+		stage.update();
+	});
+	containers[3].on("click", function (evt) {
+		changeDescription(3);
+		stage.update();
+	});
+	containers[4].on("click", function (evt) {
+		changeDescription(4);
+		stage.update();
+	});
+	containers[5].on("click", function (evt) {
+		changeDescription(5);
+		stage.update();
+	});
+	containers[6].on("click", function (evt) {
+		changeDescription(6);
+		stage.update();
+	});
+	containers[7].on("click", function (evt) {
+		changeDescription(7);
+		stage.update();
+	});
+	containers[8].on("click", function (evt) {
+		changeDescription(8);
+		stage.update();
+	});
+	containers[9].on("click", function (evt) {
+		changeDescription(9);
+		stage.update();
+	});
+	containers[10].on("click", function (evt) {
+		changeDescription(10);
+		stage.update();
+	});
+	containers[11].on("click", function (evt) {
+		changeDescription(11);
+		stage.update();
+	});
+	containers[12].on("click", function (evt) {
+		changeDescription(12);
+		stage.update();
+	});
+	containers[13].on("click", function (evt) {
+		changeDescription(13);
+		stage.update();
+	});
+	containers[14].on("click", function (evt) {
+		changeDescription(14);
+		stage.update();
+	});
+	containers[15].on("click", function (evt) {
+		changeDescription(15);
+		stage.update();
+	});
+	containers[16].on("click", function (evt) {
+		changeDescription(16);
+		stage.update();
+	});
+	containers[17].on("click", function (evt) {
+		changeDescription(17);
+		stage.update();
+	});
+	containers[18].on("click", function (evt) {
+		changeDescription(18);
+		stage.update();
+	});
+	containers[19].on("click", function (evt) {
+		changeDescription(19);
+		stage.update();
+	});
+	containers[20].on("click", function (evt) {
+		changeDescription(20);
+		stage.update();
+	});
+	containers[21].on("click", function (evt) {
+		changeDescription(21);
+		stage.update();
+	});
+	containers[22].on("click", function (evt) {
+		changeDescription(22);
+		stage.update();
+	});
+	containers[23].on("click", function (evt) {
+		changeDescription(23);
+		stage.update();
+	});
+	containers[24].on("click", function (evt) {
+		changeDescription(24);
+		stage.update();
+	});
+	
+
+	stage.update();
+
+}
+
+
+function filter(pos1, pos2) {
+	var arr = new Array();
+	for (var i = 0; i < ANTICUERPOSCOUNT; i++) {
+		if (arrAnticuerpos[i].fin > pos1 && arrAnticuerpos[i].inicio < pos2) {
+			console.log(i);
+			arr.push(arrAnticuerpos[i]);
+		}
+	}
+	return arr;
+}
+
+function repopulateTable(arr) {
+	var tabla = $("table.tablaProteinas");
+	l = arr.length;
+	tabla.html("<tr><th>mAb</th><th>Start</th><th>End</th><th>Specific strain: GP region</th><th>Percentage of conservation</th><th>Epitope type</th></tr>");
+	for (var ii = 0; ii < l; ii++) {
+		var builtHtml = "<tr><td>" + arr[ii].mAb + "</td><td>" + arr[ii].inicio + "</td><td>" + arr[ii].fin + "</td><td>" + arr[ii].GPRegion + "</td><td>" + arr[ii].Conservation + "</td><td>" + arr[ii].EpType + "</td></tr>";
+		tabla.html(tabla.html() + builtHtml);
+	}
+}
+
+
+function changeDescription(num) {
+	var hdr = $("#prothead");
+	hdr.html("mAb: " + arrAnticuerpos[num].mAb);
+	var section = $("#protpar");
+	section.html("<p><hr />start: " + arrAnticuerpos[num].inicio + "<hr />end: " + arrAnticuerpos[num].fin + "<hr />GP Region: " + arrAnticuerpos[num].GPRegion + "<hr />Conservation: " + arrAnticuerpos[num].Conservation + "<hr />EpType: " + arrAnticuerpos[num].EpType +  "</p>");
+}
+
+
+function getColor(num) {
+	switch (num) {
+	case 0:
+	case 11:
+		return "#a6cee3";
+		break;
+	case 1:
+	case 12:
+		return "#1f78b4";
+		break;
+	case 2:
+	case 13:
+		return "#b2df8a";
+		break;
+	case 3:
+	case 14:
+		return "#33a02c";
+		break;
+	case 4:
+	case 15:
+		return "#fb9a99";
+		break;
+	case 5:
+	case 16:
+		return "#e31a1c";
+		break;
+	case 6:
+	case 17:
+		return "#fdbf6f";
+		break;
+	case 7:
+	case 18:
+		return "#cab2d6";
+		break;
+	case 8:
+	case 19:
+		return "#6a3d9a";
+		break;
+	case 9:
+	case 20:
+		return "#ffff99";
+		break;
+	case 10:
+	case 21:
+		return "#b15928";
+		break;
+	default:
+		return "#ff7f00";
+		break;
+	}
+}
--- Javascript/canvasDrawings.js
+++ Javascript/canvasDrawings.js
@@ -1,14 +1,15 @@
-function Caracteristica(gpc, inicio, fin) {
+function Caracteristica(gpc, inicio, fin, nivel, order) {
 	this.gpc = gpc;
 	this.inicio = inicio;
 	this.fin = fin;
+	this.nivel = nivel;
 }
 
-var caracteristicasProteina = [new Caracteristica("GP1", 1, 501), new Caracteristica("sGP", 1, 364), new Caracteristica("GP2", 502, 676), new Caracteristica("GP2 DELTA", 502, 637), new Caracteristica("RECEPTOR BINDING", 54, 201), new Caracteristica("MUCIN LIKE REGION", 305, 485), new Caracteristica("MFL", 393, 556), new Caracteristica("PEPTIDE FUSION", 524, 539)];
+var caracteristicasProteina = [new Caracteristica("GP1", 1, 501, 1), new Caracteristica("sGP", 1, 364, 2), new Caracteristica("GP2", 502, 676, 1), new Caracteristica("GP2 DELTA", 502, 637, 4), new Caracteristica("RECEPTOR BINDING", 54, 201, 3), new Caracteristica("MUCIN LIKE REGION", 305, 485, 4), new Caracteristica("MFL", 393, 556, 2), new Caracteristica("PEPTIDE FUSION", 524, 539, 3)];
 
 $(document).ready(function () {
 	var tabla = $("table.tablaProteinas");
-	tabla.html("<tr><th>GP Completa</th><th>Inicio</th><th>Final</th></tr>");
+	tabla.html("<tr><th>Full GP</th><th>Start</th><th>End</th></tr>");
 	for (var ii = 0; ii < 8; ii++) {
 		var builtHtml = "<tr><td>" + caracteristicasProteina[ii].gpc + "</td><td>" + caracteristicasProteina[ii].inicio + "</td><td>" + caracteristicasProteina[ii].fin + "</td></tr>";
 		tabla.html(tabla.html() + builtHtml);
@@ -19,203 +20,126 @@ var ANCHO = 70;
 var ANCHOTOTAL = 80;
 var ESPACIADO = 5;
 var RADIO = 10;
+var TEXT = "bold 14px Arial";
+var TEXTCOLOR = "#000000";
+
 
 
 function init() {
 	// Crear el stage para dibujar sobre él
-	var stage = new createjs.Stage("demoCanvas");
-	var i = -1;
+	var stage = new createjs.Stage("drawingCanvas");
 	var GP1 = new createjs.Container();
 	var r = new createjs.Shape();
-	i++;
-	
+
+	// Crear variables para las funciones que dibujan rectángulos de selección
+	var rec = new createjs.Shape();
+	var rec2 = new createjs.Shape();
+	var posIni;
+	var posFin;
+
+	// Crear arreglos de containers
+	var containers = new Array();
+	var shapes = new Array();
+	var tags = new Array();
+
+
+	// Crear GP superior
 	var MAINGP = new createjs.Container();
 	var GPSHAPE = new createjs.Shape();
-	GPSHAPE.graphics.beginFill("#cab2d6").drawRoundRect(0, ESPACIADO, 700, ANCHO, RADIO);
-	var GPLABEL = new createjs.Text("GP", "bold 14px Arial", "#000000");
+	GPSHAPE.graphics.beginFill("#cab2d6").drawRoundRect(0, ESPACIADO, 680, ANCHO, RADIO);
+	var GPLABEL = new createjs.Text("GP", TEXT, TEXTCOLOR);
 	GPLABEL.textAlign = "center";
-	GPLABEL.y = ANCHOTOTAL/2 - 7;
-	GPLABEL.x = 350;
-	
-	r.graphics.beginFill(getColor(i)).drawRoundRect(caracteristicasProteina[i].inicio, ANCHOTOTAL + ESPACIADO, caracteristicasProteina[i].fin - caracteristicasProteina[i].inicio, ANCHO, RADIO);
-	var label = new createjs.Text(caracteristicasProteina[i].gpc, "bold 14px Arial", "#000000");
-	label.textAlign = "center";
-	label.y = ANCHOTOTAL + ANCHOTOTAL/2 - 7;
-	label.x = 250;
+	GPLABEL.y = ANCHOTOTAL / 2 - 7;
+	GPLABEL.x = 340;
 
-	var sGP = new createjs.Container();
-	var r2 = new createjs.Shape();
-	i++;
-	r2.graphics.beginFill(getColor(i)).drawRoundRect(caracteristicasProteina[i].inicio, ANCHOTOTAL*2 + ESPACIADO, caracteristicasProteina[i].fin - caracteristicasProteina[i].inicio, ANCHO, RADIO);
-	var label2 = new createjs.Text(caracteristicasProteina[i].gpc, "bold 14px Arial", "#000000");
-	label2.textAlign = "center";
-	label2.y = ANCHOTOTAL*2 + ANCHOTOTAL/2 - 7;
-	label2.x = 182;
-	
-	var GP2 = new createjs.Container();
-	var r3 = new createjs.Shape();
-	i++;
-	r3.graphics.beginFill(getColor(i)).drawRoundRect(caracteristicasProteina[i].inicio, ANCHOTOTAL + ESPACIADO, caracteristicasProteina[i].fin-caracteristicasProteina[i].inicio, ANCHO, RADIO);
-	var label3 = new createjs.Text(caracteristicasProteina[i].gpc, "bold 14px Arial", "#000000");
-	label3.textAlign = "center";
-	label3.y = ANCHOTOTAL + ANCHOTOTAL/2 - 7;
-	label3.x = 586;
-	
-	var GP2Delta = new createjs.Container();
-	var r4 = new createjs.Shape();
-	i++;
-	r4.graphics.beginFill(getColor(i)).drawRoundRect(caracteristicasProteina[i].inicio, ANCHOTOTAL*4 + ESPACIADO, caracteristicasProteina[i].fin-caracteristicasProteina[i].inicio, ANCHO, RADIO);
-	var label4 = new createjs.Text(caracteristicasProteina[i].gpc, "bold 14px Arial", "#000000");
-	label4.textAlign = "center";
-	label4.y = ANCHOTOTAL*4 + ANCHOTOTAL/2 - 7;
-	label4.x = (637 - 502) + 435;
-	
-	var Receptor = new createjs.Container();
-	var r5 = new createjs.Shape();
-	i++;
-	r5.graphics.beginFill(getColor(i)).drawRoundRect(caracteristicasProteina[i].inicio, ANCHOTOTAL*3 + ESPACIADO, caracteristicasProteina[i].fin - caracteristicasProteina[i].inicio, ANCHO, RADIO);
-	var label5 = new createjs.Text(caracteristicasProteina[i].gpc, "bold 14px Arial", "#000000");
-	label5.textAlign = "center";
-	label5.y = ANCHOTOTAL*3 + ANCHOTOTAL/2 - 7;
-	label5.x = (201 - 54) - 20;
-	
-	var Mucin = new createjs.Container();
-	var r6 = new createjs.Shape();
-	i++;
-	r6.graphics.beginFill(getColor(i)).drawRoundRect(caracteristicasProteina[i].inicio, ANCHOTOTAL*4 + ESPACIADO, caracteristicasProteina[i].fin -305, ANCHO , RADIO);
-	var label6 = new createjs.Text(caracteristicasProteina[i].gpc, "bold 14px Arial", "#000000");
-	label6.textAlign = "center";
-	label6.y = ANCHOTOTAL*4 + ANCHOTOTAL/2 - 7;
-	label6.x = 395;
-	
-	var MFL = new createjs.Container();
-	var r7 = new createjs.Shape();
-	i++;
-	r7.graphics.beginFill(getColor(i)).drawRoundRect(caracteristicasProteina[i].inicio, ANCHOTOTAL*2 + ESPACIADO, caracteristicasProteina[i].fin - caracteristicasProteina[i].inicio, ANCHO , RADIO);
-	var label7 = new createjs.Text(caracteristicasProteina[i].gpc, "bold 14px Arial", "#000000");
-	label7.textAlign = "center";
-	label7.y = ANCHOTOTAL*2 + ANCHOTOTAL/2 - 7;
-	label7.x = 473;
-	
-	var PEPTIDE = new createjs.Container();
-	var r8 = new createjs.Shape();
-	i++;
-	r8.graphics.beginFill(getColor(i)).drawRoundRect(caracteristicasProteina[i].inicio, ANCHOTOTAL*3 + ESPACIADO, caracteristicasProteina[i].fin - caracteristicasProteina[i].inicio, ANCHO , RADIO);
-	var label8 = new createjs.Text(caracteristicasProteina[i].gpc, "bold 14px Arial", "#000000");
-	label8.textAlign = "center";
-	label8.y = ANCHOTOTAL*3 + ANCHOTOTAL/2 - 7;
-	label8.x = 520;
-	
 	MAINGP.addChild(GPSHAPE, GPLABEL);
-	GP1.addChild(r, label);
-	sGP.addChild(r2, label2);
-	GP2.addChild(r3, label3);
-	GP2Delta.addChild(r4, label4);
-	Receptor.addChild(r5, label5);
-	Mucin.addChild(r6, label6);
-	MFL.addChild(r7, label7);
-	PEPTIDE.addChild(r8, label8);
-
 	stage.addChild(MAINGP);
-	stage.addChild(GP1);
-	stage.addChild(sGP);
-	stage.addChild(GP2);
-	stage.addChild(GP2Delta);
-	stage.addChild(Receptor);
-	stage.addChild(Mucin);
-	stage.addChild(MFL);
-	stage.addChild(PEPTIDE);
-	
-	changeDescription(0);
+	// Termina creación de GP superior 
 
+	for (var z = 0; z < caracteristicasProteina.length; z++) {
+		var j = caracteristicasProteina[z];
+		tags.push(new createjs.Text(j.gpc, TEXT, TEXTCOLOR));
+		shapes.push(new createjs.Shape());
+		containers.push(new createjs.Container());
+
+		shapes[z].graphics.beginFill(getColor(z)).drawRoundRect(j.inicio, ANCHOTOTAL * j.nivel + ESPACIADO, j.fin - j.inicio, ANCHO, RADIO);
+
+		tags[z].textAlign = "center";
+
+		tags[z].y = ANCHOTOTAL * j.nivel + ANCHOTOTAL / 2 - 7;
+		tags[z].x = (j.fin + j.inicio) / 2;
+		containers[z].addChild(shapes[z], tags[z]);
+
+	}
+
+	for (var j = 0; j < caracteristicasProteina.length; j++) {
+		var z = containers[j];
+		stage.addChild(z);
+	}
+	changeDescription(0);
 
 	// Agregar listener de eventos
 	stage.on("stagemousedown", mouseDown);
-
-	// Crear variables para las funciones
-	var rec = new createjs.Shape();
-	var rec2 = new createjs.Shape();
+	stage.on("stagemouseup", mouseUp);
 
 	stage.addChild(rec);
 	stage.addChild(rec2);
 
-	var posIni;
-	var posFin;
 
 	function mouseDown(event) {
-
 		rec.graphics.clear();
 		rec2.graphics.clear();
 		posIni = event.stageX;
 		rec.graphics.beginFill("rgba(0, 0, 0, .25").drawRect(0, 0, posIni, 500);
 
-
 		stage.addChild(rec);
-
-
 		stage.update();
-
-
 	};
 
-	stage.on("stagemouseup", mouseMove);
-
-
 	// Esto sucede al momento de que el click es liberado
-	function mouseMove(event) {
+	function mouseUp(event) {
 		rec2.graphics.clear();
 		posFin = event.stageX;
 		if (posFin > posIni) {
 			rec2.graphics.beginFill("rgba(0, 0, 0, .25").drawRect(posFin, 0, 700 - posFin, 500);
 			var x = filter(posIni, posFin);
 			repopulateTable(x);
-
 		} else {
 			rec.graphics.clear();
-
 		}
 		stage.addChild(rec2);
 		stage.update();
 	};
 
-
-	GP1.on("click", function (evt) {
+	containers[0].on("click", function (evt) {
 		changeDescription(0);
 		stage.update();
 	});
-	
-	
-	sGP.on("click", function (evt) {
+	containers[1].on("click", function (evt) {
 		changeDescription(1);
 		stage.update();
 	});
-	
-	GP2.on("click", function (evt) {
+	containers[2].on("click", function (evt) {
 		changeDescription(2);
 		stage.update();
 	});
-	
-	GP2Delta.on("click", function (evt) {
+	containers[3].on("click", function (evt) {
 		changeDescription(3);
 		stage.update();
 	});
-	
-	Receptor.on("click", function (evt) {
+	containers[4].on("click", function (evt) {
 		changeDescription(4);
 		stage.update();
 	});
-	
-	Mucin.on("click", function (evt) {
+	containers[5].on("click", function (evt) {
 		changeDescription(5);
 		stage.update();
 	});
-	
-	MFL.on("click", function (evt) {
+	containers[6].on("click", function (evt) {
 		changeDescription(6);
 		stage.update();
 	});
-	
-	PEPTIDE.on("click", function (evt) {
+	containers[7].on("click", function (evt) {
 		changeDescription(7);
 		stage.update();
 	});
@@ -225,7 +149,7 @@ function init() {
 }
 
 
-filter = function (pos1, pos2) {
+function filter(pos1, pos2) {
 	var l = caracteristicasProteina.length;
 	var arr = new Array();
 	for (var i = 0; i < l; i++) {
@@ -236,9 +160,9 @@ filter = function (pos1, pos2) {
 	return arr;
 }
 
-repopulateTable = function (arr) {
+function repopulateTable(arr) {
 	var tabla = $("table.tablaProteinas");
-	tabla.html("<tr><th>GP Completa</th><th>Inicio</th><th>Final</th></tr>");
+	tabla.html("<tr><th>Full GP</th><th>Start</th><th>End</th></tr>");
 	for (var ii = 0; ii < arr.length; ii++) {
 		var builtHtml = "<tr><td>" + arr[ii].gpc + "</td><td>" + arr[ii].inicio + "</td><td>" + arr[ii].fin + "</td></tr>";
 		tabla.html(tabla.html() + builtHtml);
@@ -246,38 +170,38 @@ repopulateTable = function (arr) {
 }
 
 
-changeDescription = function (num) {
+function changeDescription(num) {
 	var hdr = $("#prothead");
-	hdr.html("gpc: " + caracteristicasProteina[num].gpc);
+	hdr.html("GPC: " + caracteristicasProteina[num].gpc);
 	var section = $("#protpar");
-	section.html("<p><hr />start: " + caracteristicasProteina[num].inicio + "<hr />end: " + caracteristicasProteina[num].fin + "</p>");
+	section.html("<p><hr />Start: " + caracteristicasProteina[num].inicio + "<hr />End: " + caracteristicasProteina[num].fin + "</p>");
 }
 
-getColor = function(num){
-	switch (num){
-			case 0:
-			return "#a6cee3";
-			break;
-			case 1:
-			return "#1f78b4";
-			break;
-			case 2:
-			return "#b2df8a";
-			break;
-			case 3:
-			return "#33a02c";
-			break;
-			case 4:
-			return "#fb9a99";
-			break;
-			case 5:
-			return "#e31a1c";
-			break;
-			case 6:
-			return "#fdbf6f";
-			break;
-			default:
-			return "#ff7f00";
-			break;
+function getColor(num) {
+	switch (num) {
+	case 0:
+		return "#a6cee3";
+		break;
+	case 1:
+		return "#1f78b4";
+		break;
+	case 2:
+		return "#b2df8a";
+		break;
+	case 3:
+		return "#33a02c";
+		break;
+	case 4:
+		return "#fb9a99";
+		break;
+	case 5:
+		return "#e31a1c";
+		break;
+	case 6:
+		return "#fdbf6f";
+		break;
+	default:
+		return "#ff7f00";
+		break;
 	}
 }
